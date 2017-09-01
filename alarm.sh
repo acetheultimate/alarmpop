a83bbd2e7670074b2250e89b0288c3eefe5be829
Yogesh <yogeshsharmas.inbox@gmail.com>
2017-09-01 11:50:06 +0530

    Moving From Local to git

---
+++ alarm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+tail /home/yogi/Development/PycharmProjects/alarmPop/out.txt;
+/home/yogi/Development/PycharmProjects/alarmPop/./alarmPop.py >> /home/yogi/Development/PycharmProjects/alarmPop/out.txt;
